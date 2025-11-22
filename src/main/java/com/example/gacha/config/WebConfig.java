aa929cca77e56846aa819588eec1d32f389ee556
YJlang <wnsgk1114@naver.com>
2025-11-22 12:55:35 +0900

    cors수정2

--- src/main/java/com/example/gacha/config/WebConfig.java
+++ src/main/java/com/example/gacha/config/WebConfig.java
@@ -23,7 +23,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins(
                         "http://localhost:3000", // React 개발 서버
                         "http://127.0.0.1:3000", // localhost의 다른 표현
-                        "https://travelgacha.netlify.app"
+                        "https://travelgacha.netlify.app",
+                        "https://gachalikealion.duckdns.org"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
